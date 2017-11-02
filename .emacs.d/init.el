0853ae61312c2503c57c770f1c11808636be4eb0
Josh W <stygian-phrygian@users.noreply.github.com>
2017-11-02 19:13:46 -0400

    removed keybinding in go-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -174,8 +174,7 @@
               (lambda ()
                 (setq compile-command "go build -v && go test -v && go vet")
                 (define-key (current-local-map) "\C-c\C-c" 'compile)
-                (add-hook 'before-save-hook 'gofmt-before-save)
-                (local-set-key (kbd "M-.") 'godef-jump)))))
+                (add-hook 'before-save-hook 'gofmt-before-save)))))
 
 
 
